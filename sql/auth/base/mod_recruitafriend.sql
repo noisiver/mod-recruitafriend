4bdd831edf35729932f0fc26b39ddf79dd1b8581
Revision <tkn963@gmail.com>
2021-12-28 15:31:07 +0100

    Fix

--- sql/auth/base/mod_recruitafriend.sql
+++ sql/auth/base/mod_recruitafriend.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `mod_recruitafriend` (
 	`recruiter` INT(10) UNSIGNED NOT NULL,
 	`referral_date` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
 	`status` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
-	PRIMARY KEY (`account_id`) USING BTREE
+	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
